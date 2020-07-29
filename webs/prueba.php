0a2abee953730694763c0d8de82db2c2ce54faa0
revan07 <52211114+revan07@users.noreply.github.com>
2020-07-29 05:39:35 -0700

    Initial commit

---
+++ webs/prueba.php
@@ -0,0 +1,9 @@
+<html>
+<title> hola </title>
+<head> hello </head>
+<body>
+<FORM ACTION="form.php" METHOD="post" ENCTYPE="multipart/form-data">
+<INPUT TYPE="file" NAME="fichero">
+</FORM>
+</body>
+</html>
